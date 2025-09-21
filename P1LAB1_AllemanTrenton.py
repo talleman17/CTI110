6f29f3ba08335c309b1a79790930b72cbe8636a0
Trenton Alleman <allemant2760@student.faytechcc.edu>
2025-09-21 11:46:38 -0700

    Add files via upload

---
+++ P1LAB1_AllemanTrenton.py
@@ -0,0 +1,7 @@
+#// Input
+First_Name = input("What is your first name? ")
+Last_Name = input("What is your last name? ")
+Last_Name +="!"
+
+# Output
+print("Hello", First_Name, Last_Name, "Welcome to CTI-110.")
